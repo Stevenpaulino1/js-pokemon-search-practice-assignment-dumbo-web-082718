2e3f5b63ab95a4d2afed27501104a03ba348701c
Steven Paulino <stevenpaulino1@hotmail.com>
2019-03-25 23:50:10 -0400

    rendering list of poke
    '

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
   function fetchPokemon() {
     fetch("http://localhost:3000/pokemon")
       .then(r => r.json())
-      .then(poke => console.log("pokemon", poke));
+      // .then(poke => console.log("pokemon", poke));
+      .then(poke => iteratePoke(poke));
+  }
+
+  function iteratePoke(pokemon) {
+    pokemon.forEach(poke => renderPoke(poke));
+  }
+
+  function renderPoke(pokemon) {
+    console.log("renderPoke", pokemon);
+    const pokemonContainer = document.querySelector("#pokemon-container");
+    const pokeUL = document.createElement("ul");
+    const pokeLi = document.createElement("li");
+    pokeLi.innerText = pokemon.name;
+    pokeUL.append(pokeLi);
+    pokemonContainer.append(pokeUL);
   }
 });
