333277bfb3f7bf8fd91b8bd4dab426ef4fc2fc6e
Steven Paulino <stevenpaulino1@hotmail.com>
2019-03-25 23:31:29 -0400

    Set up db json, fetched poke

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log(POKEMON)
-  //YOUR CODE HERE
-})
+document.addEventListener("DOMContentLoaded", () => {
+  fetchPokemon();
+  function fetchPokemon() {
+    fetch("http://localhost:3000/pokemon")
+      .then(r => r.json())
+      .then(poke => console.log("pokemon", poke));
+  }
+});
